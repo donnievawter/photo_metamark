065a2130919c6b28400bd9ea85b43c4a8964131f
Don Vawter <donvawter@mac.com>
2025-09-17 13:26:17 -0600

    mistake in metamark.sh

--- metamark.sh
+++ metamark.sh
@@ -7,6 +7,6 @@ shift 2
 docker run --rm \
   -v "$INPUT":/input \
   -v "$OUTPUT":/output \
-  ghcr.io/yourname/metamark:latest \
+  ghcr.io/donnievawter/metamark:latest \
   /entrypoint.sh --input /input --output /output "$@"
 
